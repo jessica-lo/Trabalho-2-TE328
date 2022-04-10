1d3ebc139e3336adfe35af61f9e7bc8ef7177902
Jessica Lo <81395722+jessica-lo@users.noreply.github.com>
2022-04-10 18:55:09 -0300

    Update trabalho_2.cpp

--- trabalho_2.cpp
+++ trabalho_2.cpp
@@ -1,8 +1,14 @@
-﻿/*Begining of Auto generated code by Atmel studio */
+/*Programa que realiza a leitura de entrada de dados de dois botões e armazena os dados lidos em um veotr dinâmicamente alocado
+  Para cada valor inserido, é verificado se hpa necessidade de realocar o vetor inicial
+  Os dados são apresentados por meio de um display LCD
+*/
+
+/*Begining of Auto generated code by Atmel studio */
 #include <Arduino.h>
 
 /*End of auto generated code by Atmel studio */
 
+//incluir o arquivo .h das funções de processamento
 #include "functions.h"
 //Beginning of Auto generated function prototypes by Atmel Studio
 //End of Auto generated function prototypes by Atmel Studio
@@ -13,7 +19,10 @@ void loop(){}
 
 void setup()
 {
+  //chamada da função beginLCD
   beginLCD();
+  
+  //attachInterrupt(pino de interrupção, função a ser executada, modo de interrupção)
   attachInterrupt(0, readButtons, CHANGE);
   attachInterrupt(1, readButtons, CHANGE);
 }

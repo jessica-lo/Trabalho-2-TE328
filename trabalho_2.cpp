88e1da6081008f4c4ee49f144e43de768a5dfada
Jessica Lo <81395722+jessica-lo@users.noreply.github.com>
2022-04-09 22:05:04 -0300

    Add files via upload

---
+++ trabalho_2.cpp
@@ -0,0 +1,19 @@
+﻿/*Begining of Auto generated code by Atmel studio */
+#include <Arduino.h>
+
+/*End of auto generated code by Atmel studio */
+
+#include "functions.h"
+//Beginning of Auto generated function prototypes by Atmel Studio
+//End of Auto generated function prototypes by Atmel Studio
+
+
+
+void loop(){}
+
+void setup()
+{
+  beginLCD();
+  attachInterrupt(0, readButtons, CHANGE);
+  attachInterrupt(1, readButtons, CHANGE);
+}
